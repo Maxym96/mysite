a494ee16a28efd4aaee8cbabf0497706f87e3cc2
Maksym Popov <maksym.popov@branderstudio.com>
2021-12-13 14:34:49 +0200

    Delete .idea from GitHub

--- public/index.php
+++ public/index.php
@@ -10,18 +10,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-//$productCon = new ProductController();
-//$productList = new ProductList();
-//$productCon->productList($productList->productList);
 
 $router = new Router($_SERVER['REQUEST_URI']);
 $router->run();
-
-//echo '<pre>';
-//print_r($_SERVER['REQUEST_URI']);
-//echo '</pre>';
-
-
-//$app = new App();
-//$app->App();
-//echo 'My site is the best things';
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,12 +6,6 @@ use App\Services\ISession;
 
 class ProductController extends ProductList
 {
-
-//    public function __construct(array $productList)
-//    {
-//        $this->$productList = $productList;
-//    }
-
     public function productList(): void
     {
         foreach ($this->getFile() as $key=>$item) {
